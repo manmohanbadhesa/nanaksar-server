afcf9c9580ca9555425163a30260a322d4b2ffd1
manmohanbadhesa <manmohanb1995@gmail.com>
2024-05-22 17:16:21 -0700

    debugging mysql on heidi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 // loading variable from .env
 require("dotenv").config({ path: "./.env" });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 // import routes
 const paymentFiveRoutes = require("./routes/paymentFiveRoutes");
